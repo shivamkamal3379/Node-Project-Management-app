669793d412e6a600cfb64773c57b559502064f8d
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-27 19:37:46 +0530

    dot env structure done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Start of a backend project");
-// comment
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
+
+let myusername = process.env.database ;
+console.log("value is " , myusername);
+
+
+console.log("begining of a backend project");
+console.log("nodemon is working by auto refreshing my server");
