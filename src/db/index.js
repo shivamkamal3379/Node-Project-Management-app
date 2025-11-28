acf0934e06332a6f681e1bf8db117542700d4a76
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-28 16:56:30 +0530

    Mongo DB Config

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+
+const connectDB = async () => {
+
+    try{
+       await mongoose.connect(process.env.MONGO_URI)
+       console.log("✅Mongo DB Connected")
+    }catch(error){
+        console.error(" ❎mongoose Connection error ");
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
