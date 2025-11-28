632e0da48c4b8154639117052560af906486f1b7
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-28 12:16:39 +0530

    CORS and DT ENV set up

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express"
+import  cors from "cors"
+ 
+
+const app = express()
+
+ // basic configurations 
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended:true , limit : "16kb"}))
+app.use(express.static("public"))
+
+
+// cors configurations 
+app.use(cors({
+    origin : process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials : true , 
+    methods : [ "GET " ,"POST" , "PUT" , "PATCH" , "DELETE" , "OPTIONS"], 
+    allowedHeaders : [ "Content-Type " , "Authorization"] , 
+    
+}))
+
+
+
+app.get ("/" ,  (req, res) => {
+    res.send("welcome to testing of basecampy");
+
+
+});
+
+ export default app 
