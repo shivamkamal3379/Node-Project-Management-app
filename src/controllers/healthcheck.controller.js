b4cf00c8741709ffd6091349ec844ca11521b2fb
Shivam Kamal <shivamkamal3379@gmail.com>
2025-11-29 19:37:56 +0530

    Async Handler sorted

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,13 +1,25 @@
 import { ApiResponse } from "../utils/api-response.js";
-
-
- const healthCheck = (req , res ) => {
+import { asyncHandler } from "../utils/async-handler.js";
+/**
+ const healthCheck = async (req , res , next) => {
     try{
+        const user =  await getUserFromDB()
         res.status(200).json(
             new ApiResponse (200 , {mesage : " Server is Running "}));
     }catch(error){
+        next (err);
 
     }
  }
+ */
 
+
+
+
+
+ const healthCheck = asyncHandler(async (req, res)=> {
+    res.status(200).json(
+        new ApiResponse(200 , {message : "Server is Running"})
+    )
+ })
  export {healthCheck};
