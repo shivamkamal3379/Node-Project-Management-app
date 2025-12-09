d69536e4a4c953635867b7249f5af13d71ee3450
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-10 00:14:18 +0530

    Used Nodemailer For the Mail Sending Still structure designed and Error handling Done

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,40 @@
 import mailgen from "mailgen"
+import nodemailer from "nodemailer"
+
+const sendEmail = async (options) => {
+     const mailGenerator = new Mailgen({
+        theme : "default" ,
+        product : {
+            name : "Task Manager" , 
+            link: "https://taskmanagerlink.com"
+        }
+    })
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent)
+    const emailHtml = mailGenerator.generate(options.mailgenContent)
+
+
+   const transporter = nodemailer.createTransport({
+        host : process.env.MAILTRAP_SMTP_HOST,
+        port: MAILTRAP_SMTP_PORT,
+        auth:{
+            user : process.env.MAILTRAP_SMTP_USER,
+            pass : process.env.MAILTRAP_SMTP_PASS
+        }
+    })
+    const mail = {
+        from :"mail.yaskmanager@example.com",
+        to : options.email , 
+        subject : options.subject,
+        text: emailTextual,
+        html: emailHtml
+    }
+    try{
+        await transporter.sendMail(mail)
+    } catch(error){
+        console.error("Email Service failed silently. Make sure you have provided your MAILTRAP credentials in the .env file");
+        console.error("Error" , error);
+    }
+}
 
 const emailVerificationMailGenContent = (username , verificationUrl ) =>{
     return {
@@ -42,5 +78,6 @@ const forgotPasswordMailGenContent = (username , passwordResetUrl ) =>{
 
 export {
     emailVerificationMailGenContent , 
-    forgotPasswordMailGenContent   
+    forgotPasswordMailGenContent    , 
+    sendEmail,
 }
