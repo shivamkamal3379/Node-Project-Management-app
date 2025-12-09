92d4196b0db38eb5d8f9201469d0001a9e9ddc4a
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-09 22:39:01 +0530

    Initialized JWT Tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,9 @@
 import mongoose , {Schema}from "mongoose " ; 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
 const userSchema = new Schema (
-    {
+    { 
         avatar: {
             type:{
                 url : String , 
@@ -72,8 +74,31 @@ userSchema.methods.isPasswordCorrect = async function (password){
 };
 
 
-//done 
+userSchema.methods.generateAccessToken = function (){
+   return  jwt.sign(
+        {
+            _id : this._id ,
+            email: this.email,
+            username : this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET , 
+        {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
 
+
+userSchema.methods.generateRefreshToken = function(){
+        jwt.sign(
+            {
+                _id : this._id , 
+                mail: this.email,
+                username : this.username
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {expiresIn : process.env.REFRESH_TOKEN_EXPIRY}
+
+        )
+}
 
 
 export const User = mongoose.model("User" , userSchema)
