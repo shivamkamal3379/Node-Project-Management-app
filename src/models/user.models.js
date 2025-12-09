d5019307391b513f2c480da99099f668dc0f21dc
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-09 18:47:49 +0530

    Setting Prehooks

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose , {Schema}from "mongoose " ; 
+import bcrypt from "bcrypt";
 const userSchema = new Schema (
     {
         avatar: {
@@ -60,7 +61,18 @@ const userSchema = new Schema (
         timeStamps: true , 
     }
 )
+userSchema.pre("save" , async function (next){
+   if(!this.isModified("password"))return next()
+    this.password =  await bcrypt.hash(this.password , 10 )
+    next()
+})
+
+
+
 //done 
+
+
+
 export const User = mongoose.model("User" , userSchema)
 
 
