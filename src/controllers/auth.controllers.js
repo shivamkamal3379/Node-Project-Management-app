eb7730f9f3aba0a0562b60ae7046cbd8b064b48a
Shivam Kamal <shivamkamal3379@gmail.com>
2026-01-05 12:17:08 +0530

    Refresh tokens and AccessTokens in logging in a user

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,12 +1,16 @@
 import { User } from "../models/user.models.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
+import {isPasswordCorrect} from "../models/user.models.js"
 import { asyncHandler } from "../utils/async-handler.js";
+
 import {
   emailVerificationMailGenContent,
   forgotPasswordMailGenContent,
   sendEmail,
 } from "../utils/mail.js";
+
+
 import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
@@ -79,4 +83,56 @@ const registerUser = asyncHandler(async (req, res) => {
       ),
     );
 });
-export{registerUser};
+
+
+const login = asyncHandler(async (req, res)=>{
+  const {email , password , username} = req.body
+
+  if( !email){
+    throw new ApiError(400 , "Username or email is required")
+  }
+
+  const user = await User.findOne({email});
+  if(!user){
+    throw new ApiError(400 , "Username or email is required")
+
+  }
+
+  const isPasswordValid  =  await user.isPasswordCorrect(password);
+  if(!isPasswordValid){
+    throw new ApiError(400 , "Invalid Credentials ")
+  }
+  const {accessToken , refreshToken}  = await generateAccessAndRefreshTokens(user._id)
+
+  const LoggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+
+  const options = {
+    httpOnly : true ,
+    secure: true 
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken , options)
+  .cookie("refreshToken", refreshToken , options)
+  .json(
+    new ApiResponse(
+    200 , 
+    {
+      user: loggedInUser , 
+      accessToken , 
+      refreshToken
+    },
+    "User Logged In Successsfully "
+
+  )
+
+
+)
+});
+
+
+
+export{registerUser , login};
