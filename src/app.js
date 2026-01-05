61ce50d886ee557f66e430bf2e47ccdb060ea9e5
Shivam Kamal <shivamkamal3379@gmail.com>
2026-01-05 12:17:29 +0530

    Login with tokens and cookie data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import  cors from "cors"
+import cookieParser from "cookie-parser"
  
 
 const app = express()
@@ -9,6 +10,7 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended:true , limit : "16kb"}))
 app.use(express.static("public"))
 
+app.use(cookieParser());
 
 // cors configurations 
 app.use(cors({
---
+++ src/validators/index.js
@@ -0,0 +1,37 @@
+import {body} from "express-validator";
+
+
+const userRegisterValidator = ()=>{
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+            
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage()
+            .withMessage("Username is  Required")
+            .isLowercase()
+            .withMessage("Usernme must be in lowercase")
+            .isLength({min:3})
+            .withMessage("Username must be at least 3 characters long"),
+
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is Required"),
+            bosy("Full name ")
+            .optional()
+            .trim()
+
+    ]
+}
+
+
+export {
+    userRegisterValidator
+}
